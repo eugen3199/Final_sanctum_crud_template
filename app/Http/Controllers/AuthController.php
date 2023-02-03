f4fc1d7b392ffe25f001bf63c7fa1174c402d411
eugen3199 <3199busi@gmail.com>
2023-02-04 04:14:41 +0630

    DB connection string on model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,11 +27,11 @@ class AuthController extends Controller
             $client="mysql2";
         }
 
-        $user = Users::create([
+        $user = Users::on($client)->create([
             'name' => $fields['name'],
             'email' => $fields['email'],
             'password' => bcrypt($fields['password'])
-        ])->setConnectionto($client);
+        ]);
 
         $token = $user->createToken('kbtc_oid')->plainTextToken;
 
