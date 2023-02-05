31cdce636cb30993cdba8623a0866e5509a2e9f8
eugen3199 <3199busi@gmail.com>
2023-02-05 11:24:28 +0630

    Bug fix

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Students;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Support\Facades\Storage;
 
 class StudentController extends Controller
